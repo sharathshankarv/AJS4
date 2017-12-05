170d46e81799fb13eb8409415ef47bb20f53cea7
Sharath Shankar V <vsshankar@tpcsed.com>
2017-12-05 10:38:03 +0530

    pipe filters added

--- src/app/two-way-binding/two-way-binding.component.ts
+++ src/app/two-way-binding/two-way-binding.component.ts
@@ -4,12 +4,26 @@ import { Component } from '@angular/core';
   selector: 'app-two-way-binding',
   template: `
               <input [(ngModel)]="email" (keyup.enter)="displayValue()" />
+              <br/><br/>
+              <p>{{courseDet.title | uppercase }}</p>
+              <p>{{courseDet.trainer | lowercase}}</p>
+              <p>{{courseDet.strength | number}}</p>
+              <p>{{courseDet.date | date:'shortDate'}}</p>
+              <p>{{courseDet.price | currency:"INR"}}</p>
             `,
   styleUrls: ['./two-way-binding.component.css']
 })
 export class TwoWayBindingComponent{
 
   email = "sharath@gmail.com"
+
+  courseDet = {
+    "title" : "AngularJs",
+    "trainer" : "Mosh",
+    "strength" : "30897",
+    "date" : new Date(2017,5,11),
+    "price" : 190
+  }
 
   displayValue(){
     console.log(this.email);
